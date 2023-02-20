ae9d647c0ea6f22f5c8ced00b10c2cbb239305cb
JP <sreeprakash210503@gmail.com>
2023-02-20 10:53:03 +0530

    Created a App class successfully

---
+++ lib/views/homepage.dart
@@ -0,0 +1,16 @@
+//create a homepage for debug
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crud App'),
+      ),
+      body: Container(),
+    );
+  }
+}
