1e3e97a8f93f53965e6a04426aefc4f1b6cb70f7
JP <sreeprakash210503@gmail.com>
2023-02-20 12:23:18 +0530

    added changes for adding a id in model

--- lib/model/person_details.dart
+++ lib/model/person_details.dart
@@ -3,16 +3,19 @@ class Persondetails
 {
   String? name;
   int? age;
+  String? id;
 
   Persondetails({
     this.name,
     this.age,
+    this.id
   });
 
 //from json 
   Persondetails.fromJson(Map<String, dynamic> json) {
     name = json['name'];
     age = json['age'];
+    id=json['_id'];
   }
 
 //to json 
@@ -20,6 +23,7 @@ class Persondetails
     final Map<String, dynamic> data = <String, dynamic>{};
     data['name'] = name;
     data['age'] = age;
+    data['_id']=id;
     return data;
   }
 }
